530443417622998bd408e22f0dfbad725922211c
Tanapat <tanapat.psr@mail.kmutt.ac.th>
2022-09-11 13:11:43 +0700

    test2

--- minMedMax.js
+++ minMedMax.js
@@ -1,11 +1,12 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
+let array0 ;
 let array = [n1,n2,n3]
 for (let i = 0; i < array.length; i++) {
   for (let x = i + 1; x < array.length; x++) {
     if(array[i] > array[x]) {
-    let array0 ;
+    
     array0 = array[i];
     array[i] = array[x];
     array[x]= array0;
@@ -15,6 +16,6 @@ for (let i = 0; i < array.length; i++) {
 return {min:array[0], mid: array[1], max: array[2]}
 }
 
-console.log(minMedMax(85,30,1));
+
 
 module.exports = minMedMax
